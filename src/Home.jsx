b402091146750e45299935bb4fafc2a99b591477
ahmadnurain <ahmadnurain82@gmail.com>
2024-12-20 19:41:03 +0700

    delete marquee.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,7 +7,6 @@ import BlurFade from "@/components/magicui/blur-fade";
 
 import OurMenu from "./OurMenu";
 import ContactUs from "./Contactus";
-import { MarqueeDemo } from "./Marquee";
 
 const scrollVariants = {
   hidden: { opacity: 0, y: 50 },
@@ -55,9 +54,6 @@ function Home() {
       </section>
       <section id="contact">
         <ContactUs />
-      </section>
-      <section>
-        <MarqueeDemo />
       </section>
     </>
   );
